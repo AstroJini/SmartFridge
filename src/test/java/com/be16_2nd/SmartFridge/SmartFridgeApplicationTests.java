ef56d1cfe747c55d0ba04fbf784d3e0ce5e8611f
YoungKwanK <young.kwin@gmail.com>
2025-07-24 17:52:43 +0900

    [추가] 프로젝트 생성
    원인 :
    수정사항 :
    영향 범위 :

---
+++ src/test/java/com/be16_2nd/SmartFridge/SmartFridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be16_2nd.SmartFridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
