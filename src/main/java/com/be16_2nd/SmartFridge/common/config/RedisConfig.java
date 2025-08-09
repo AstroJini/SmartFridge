61e5a775da5f6e9c5d65f8a8dbfdde0e4afacb88
Jihyeon0804 <cam080421@naver.com>
2025-08-10 03:03:31 +0900

    [추가] Redis Configuration

---
+++ src/main/java/com/be16_2nd/SmartFridge/common/config/RedisConfig.java
@@ -0,0 +1,83 @@
+package com.be16_2nd.SmartFridge.common.config;
+
+import com.be16_2nd.SmartFridge.common.service.NotificationSubscriber;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    @Qualifier("rtInventory")
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        configuration.setDatabase(0);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    @Qualifier("rtInventory")
+    public RedisTemplate<String, String> redisTemplate(@Qualifier("rtInventory") RedisConnectionFactory redisConnectionFactory) {          // 템플릿 객체
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+
+    @Bean
+    @Qualifier("ssePubSub")
+    public RedisConnectionFactory sseFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    @Qualifier("ssePubSub")
+    public RedisTemplate<String, String> sseRedisTemplate(@Qualifier("ssePubSub") RedisConnectionFactory redisConnectionFactory) {          // 템플릿 객체
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+
+    @Bean
+    @Qualifier("ssePubSub")
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            @Qualifier("ssePubSub") RedisConnectionFactory redisConnectionFactory
+            , MessageListenerAdapter messageListenerAdapter) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(messageListenerAdapter, new PatternTopic("sse-channel"));
+        return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter messageListenerAdapter(NotificationSubscriber notificationSubscriber) {
+        return new MessageListenerAdapter(notificationSubscriber, "onMessage");
+    }
+}
