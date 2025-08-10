bd8a6cac90b82ad228272a2b9c0df1510905a378
Jihyeon0804 <cam080421@naver.com>
2025-08-10 14:37:04 +0900

    [추가] Notification

--- src/main/java/com/be16_2nd/SmartFridge/notification/domain/Notification.java
+++ src/main/java/com/be16_2nd/SmartFridge/notification/domain/Notification.java
@@ -4,6 +4,8 @@ import com.be16_2nd.SmartFridge.Post.domain.Post;
 import com.be16_2nd.SmartFridge.common.domain.BaseTimeEntity;
 import com.be16_2nd.SmartFridge.food.domain.Food;
 import com.be16_2nd.SmartFridge.fridge.domain.Fridge;
+import com.be16_2nd.SmartFridge.inquiry.domain.Inquiry;
+import com.be16_2nd.SmartFridge.inquiryComment.domain.InquiryComment;
 import com.be16_2nd.SmartFridge.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.*;
@@ -47,4 +49,41 @@ public class Notification extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "inquiry_id")
+    private Inquiry inquiry;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "inquiryComment_id")
+    private InquiryComment inquiryComment;
+
+
+    public static Notification fromInquiry(Member sender, Member receiver, Inquiry inquiry) {
+        String content = sender.getName() + "님이 새로운 문의를 등록했습니다.";
+        return Notification.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .fridge(inquiry.getFridge())
+                .content(content)
+                .notificationType(NotificationType.NEW_INQUIRY)
+                .inquiry(inquiry)
+                .isRead(false)
+                .build();
+    }
+
+    public static Notification fromInquiryComment(Member sender, Member receiver, InquiryComment inquiryComment) {
+        String content = NotificationType.ADMIN_REPLY.getDescription();
+        return Notification.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .content(content)
+                .inquiry(inquiryComment.getInquiry())
+                .notificationType(NotificationType.ADMIN_REPLY)
+                .inquiryComment(inquiryComment)
+                .isRead(false)
+                .build();
+    }
+
+
 }
