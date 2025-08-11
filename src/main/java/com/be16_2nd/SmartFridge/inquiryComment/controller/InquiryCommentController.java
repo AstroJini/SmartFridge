65ca13fb7c74a4755f1e0c6deccac8af00589d18
Jihyeon0804 <cam080421@naver.com>
2025-08-11 20:11:16 +0900

    [수정] Inquiry + 알림 기능

--- src/main/java/com/be16_2nd/SmartFridge/inquiryComment/controller/InquiryCommentController.java
+++ src/main/java/com/be16_2nd/SmartFridge/inquiryComment/controller/InquiryCommentController.java
@@ -1,19 +1,29 @@
 package com.be16_2nd.SmartFridge.inquiryComment.controller;
 
+import com.be16_2nd.SmartFridge.common.dto.CommonDto;
+import com.be16_2nd.SmartFridge.inquiryComment.dto.InquiryCommentCreateDto;
+import com.be16_2nd.SmartFridge.inquiryComment.service.InquiryCommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/inquirycomment")
 public class InquiryCommentController {
 
-    public ResponseEntity<?> create(){
-        return null;
+    private final InquiryCommentService inquiryCommentService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestParam Long inquiryId
+            , @RequestBody InquiryCommentCreateDto inquiryCommentCreateDto){
+        return new ResponseEntity<>(CommonDto.builder()
+                .result(inquiryCommentService.create(inquiryCommentCreateDto, inquiryId))
+                .status_code(HttpStatus.CREATED.value())
+                .status_message("답변 등록 완료").build(), HttpStatus.CREATED);
     }
 
     @Transactional(readOnly = true)
