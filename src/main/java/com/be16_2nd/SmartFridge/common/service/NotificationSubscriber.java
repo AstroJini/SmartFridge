11ac05d72eebdce2d4c7d98c82df87e72e930fd1
Jihyeon0804 <cam080421@naver.com>
2025-08-11 19:47:42 +0900

    [수정] Notification pub/sub

--- src/main/java/com/be16_2nd/SmartFridge/common/service/NotificationSubscriber.java
+++ src/main/java/com/be16_2nd/SmartFridge/common/service/NotificationSubscriber.java
@@ -17,24 +17,22 @@ import java.io.IOException;
 public class NotificationSubscriber implements MessageListener {
 
     private final SseEmitterRegistry sseEmitterRegistry;
-    private final ObjectMapper objectMapper; // Bean으로 주입받아 사용
+    private final ObjectMapper objectMapper;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
         try {
-            // Redis에서 받은 byte 데이터를 DTO로 변환
             SseMessageDTO sseMessageDTO = objectMapper.readValue(message.getBody(), SseMessageDTO.class);
             log.info("Redis 구독 메시지 수신. 수신자: {}", sseMessageDTO.getReceiverEmail());
 
-            // 현재 서버에 접속한 Emitter가 있는지 확인
             SseEmitter sseEmitter = sseEmitterRegistry.getEmitter(sseMessageDTO.getReceiverEmail());
 
             if (sseEmitter != null) {
                 try {
-                    // Emitter가 존재하면 실시간 알림 전송
+                    String eventData = objectMapper.writeValueAsString(sseMessageDTO);
                     sseEmitter.send(SseEmitter.event()
                             .name(sseMessageDTO.getType())
-                            .data(sseMessageDTO));
+                            .data(eventData));
                     log.info("실시간 알림 전송 완료: {}", sseMessageDTO.getReceiverEmail());
                 } catch (IOException e) {
                     // 전송 중 오류 발생 시 Emitter 제거
