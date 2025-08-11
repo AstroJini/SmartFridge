0d2ae0812e6d9b10dfded8144b4af7735c439643
Jihyeon0804 <cam080421@naver.com>
2025-08-11 17:52:38 +0900

    [추가] Notification Setting

--- src/main/java/com/be16_2nd/SmartFridge/notification/domain/NotificationSetting.java
+++ src/main/java/com/be16_2nd/SmartFridge/notification/domain/NotificationSetting.java
@@ -17,7 +17,7 @@ public class NotificationSetting {
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    private SettingType type;
+    private NotificationSettingType type;
 
     private boolean isActive;
 
