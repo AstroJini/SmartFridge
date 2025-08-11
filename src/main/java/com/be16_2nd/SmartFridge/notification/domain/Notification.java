c2804de153b979b601967755461e80a637a20ce6
Jihyeon0804 <cam080421@naver.com>
2025-08-11 17:52:22 +0900

    [추가] Notification

--- src/main/java/com/be16_2nd/SmartFridge/notification/domain/Notification.java
+++ src/main/java/com/be16_2nd/SmartFridge/notification/domain/Notification.java
@@ -1,6 +1,7 @@
 package com.be16_2nd.SmartFridge.notification.domain;
 
 import com.be16_2nd.SmartFridge.Post.domain.Post;
+import com.be16_2nd.SmartFridge.Post.domain.PostComment;
 import com.be16_2nd.SmartFridge.common.domain.BaseTimeEntity;
 import com.be16_2nd.SmartFridge.food.domain.Food;
 import com.be16_2nd.SmartFridge.fridge.domain.Fridge;
@@ -81,6 +82,55 @@ public class Notification extends BaseTimeEntity {
                 .inquiry(inquiryComment.getInquiry())
                 .notificationType(NotificationType.ADMIN_REPLY)
                 .inquiryComment(inquiryComment)
+                .isRead(false)
+                .build();
+    }
+
+    // 게시글 공지사항 등록 알림
+    public static Notification fromAnnounce(Member sender, Member receiver, Post post) {
+        String content = "'" + post.getFridge().getFridgeName() + "' 냉장고에 새로운 공지사항이 등록되었습니다.";
+
+        return Notification.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .fridge(post.getFridge())
+                .content(content)
+                .notificationType(NotificationType.NEW_ANNOUNCEMENT)
+                .post(post) // 알림과 공지사항 게시글을 연결
+                .isRead(false)
+                .build();
+    }
+
+    // 게시글 댓글 알림
+    public static Notification fromComment(Member sender, Member receiver, PostComment postComment) {
+
+        Post post = postComment.getPost();
+
+        String content = sender.getName() + "님이 '" + post.getTitle() + "' 게시글에 댓글을 남겼습니다.";
+
+        return Notification.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .fridge(post.getFridge())
+                .content(content)
+                .notificationType(NotificationType.NEW_COMMENT)
+                .post(postComment.getPost())
+                .isRead(false)
+                .build();
+    }
+
+    // 식자재 등록 알림
+    public static Notification fromFood(Member sender, Member receiver, Food food) {
+
+        String content = sender.getName() + "님이 '" + food.getFridge().getFridgeName() + "' 냉장고에 '" + food.getName() + "'을(를) 등록했습니다.";
+
+        return Notification.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .fridge(food.getFridge())
+                .content(content)
+                .notificationType(NotificationType.NEW_FOOD)
+                .food(food)
                 .isRead(false)
                 .build();
     }
