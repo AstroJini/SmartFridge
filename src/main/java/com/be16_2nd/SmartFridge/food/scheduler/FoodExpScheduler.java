9d00cdd0d8437ea1340093b93dd98ef6cdcf987a
Jihyeon0804 <cam080421@naver.com>
2025-08-12 01:19:33 +0900

    [추가] 유통기한 알림 + 스케쥴러

---
+++ src/main/java/com/be16_2nd/SmartFridge/food/scheduler/FoodExpScheduler.java
@@ -0,0 +1,65 @@
+package com.be16_2nd.SmartFridge.food.scheduler;
+
+import com.be16_2nd.SmartFridge.food.domain.Food;
+import com.be16_2nd.SmartFridge.food.repository.FoodRepository;
+import com.be16_2nd.SmartFridge.fridge.domain.FridgeMember;
+import com.be16_2nd.SmartFridge.member.domain.Member;
+import com.be16_2nd.SmartFridge.notification.domain.Notification;
+import com.be16_2nd.SmartFridge.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class FoodExpScheduler  {
+
+    private final FoodRepository foodRepository;
+    private final NotificationService notificationService;
+
+    // 매일 오전 9시에 실행
+    @Scheduled(cron = "0 0 9 * * *")
+    public void notificationForExpFood() {
+
+        // 유통기한 당일
+        LocalDate today = LocalDate.now();
+        // 유통기한 하루 전 (현재 + 1일)
+        LocalDate oneDayAfter = today.plusDays(1);
+        // 유통기한 삼일 전 (현재 + 3일)
+        LocalDate threeDaysAfter = today.plusDays(3);
+
+        // 유통기한이 현재 날짜를 기준으로 당일, 하루, 삼일 후인 음식 조회
+        List<Food> expFoodList = foodRepository.findAllByExpirationDateIn(
+                List.of(today, oneDayAfter, threeDaysAfter)
+        );
+
+        for (Food food : expFoodList) {
+            
+            // 식자재 등록한 회원
+            Member receiver = food.getMember();
+            // 유통기한 임박 기간에 따른 메세지 조합
+            String daysLeftMessage = getDaysLeftMessage(food.getExpirationDateTime().toLocalDate(), today);
+            // 알림 객체 생성
+            Notification notification = Notification.fromExpiration(receiver, food, daysLeftMessage);
+            notificationService.create(notification);
+        }
+    }
+
+    private String getDaysLeftMessage(LocalDate expiryDate, LocalDate today) {
+        if (expiryDate.isEqual(today)) {
+            return "오늘까지입니다.";
+        }
+        if (expiryDate.isEqual(today.plusDays(1))) {
+            return "1일 남았습니다.";
+        }
+        if (expiryDate.isEqual(today.plusDays(3))) {
+            return "3일 남았습니다.";
+        }
+        return "";
+    }
+}
