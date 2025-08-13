a1e654675c5adc3785cc021e93d47e9f6d58b128
Jihyeon0804 <cam080421@naver.com>
2025-08-14 01:12:58 +0900

    [추가] @Builder 추가

--- src/main/java/com/be16_2nd/SmartFridge/notification/domain/NotificationSetting.java
+++ src/main/java/com/be16_2nd/SmartFridge/notification/domain/NotificationSetting.java
@@ -3,6 +3,7 @@ package com.be16_2nd.SmartFridge.notification.domain;
 import com.be16_2nd.SmartFridge.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class NotificationSetting {
 
     @Id
