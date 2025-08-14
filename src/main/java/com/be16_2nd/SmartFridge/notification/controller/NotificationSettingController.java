369ec789e1c2e068065b523b35b31d647eef44b7
Jihyeon0804 <cam080421@naver.com>
2025-08-14 19:38:17 +0900

    [수정] 알림 수신 설정 Controller

--- src/main/java/com/be16_2nd/SmartFridge/notification/controller/NotificationSettingController.java
+++ src/main/java/com/be16_2nd/SmartFridge/notification/controller/NotificationSettingController.java
@@ -6,10 +6,7 @@ import com.be16_2nd.SmartFridge.notification.service.NotificationSettingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
