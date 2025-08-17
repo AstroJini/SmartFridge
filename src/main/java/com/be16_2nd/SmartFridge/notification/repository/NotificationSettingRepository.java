d678d4e0cf0da47769b0a11455a3c35b18368c14
Jihyeon0804 <cam080421@naver.com>
2025-08-18 02:37:09 +0900

    [추가] 조회 메서드(findByMemberAndNotificationSettingType)

--- src/main/java/com/be16_2nd/SmartFridge/notification/repository/NotificationSettingRepository.java
+++ src/main/java/com/be16_2nd/SmartFridge/notification/repository/NotificationSettingRepository.java
@@ -2,12 +2,16 @@ package com.be16_2nd.SmartFridge.notification.repository;
 
 import com.be16_2nd.SmartFridge.member.domain.Member;
 import com.be16_2nd.SmartFridge.notification.domain.NotificationSetting;
+import com.be16_2nd.SmartFridge.notification.domain.NotificationSettingType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface NotificationSettingRepository extends JpaRepository<NotificationSetting, Long> {
     List<NotificationSetting> findAllByMember(Member member);
+
+    Optional<NotificationSetting> findByMemberAndNotificationSettingType(Member member, NotificationSettingType notificationSettingType);
 }
