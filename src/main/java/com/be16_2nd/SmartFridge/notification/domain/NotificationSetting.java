d6f7c14f48503fafb07c1e848253cbf6207a3e41
Jihyeon0804 <cam080421@naver.com>
2025-08-18 02:36:26 +0900

    [추가] Unique 제약 조건 (NotificationSettingType, isActive)

--- src/main/java/com/be16_2nd/SmartFridge/notification/domain/NotificationSetting.java
+++ src/main/java/com/be16_2nd/SmartFridge/notification/domain/NotificationSetting.java
@@ -2,15 +2,15 @@ package com.be16_2nd.SmartFridge.notification.domain;
 
 import com.be16_2nd.SmartFridge.member.domain.Member;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "uk_notification_setting_user_type", columnNames = {"user_id", "type"})
+})
 @Builder
 public class NotificationSetting {
 
@@ -19,12 +19,15 @@ public class NotificationSetting {
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    private NotificationSettingType notificationType;
+    @Column(nullable = false)
+    private NotificationSettingType notificationSettingType;
 
-    private boolean isActive;
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean isActive = true;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 }
 
