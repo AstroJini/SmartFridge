efa353f75b623c134834fd2326dc909b2a1c1acf
Kim Young Kwan <young.kwin@gmail.com>
2025-08-21 09:35:56 +0900

    [수정] 1대1채팅 서비스 수정 (#38)
    원인 : 냉장고 관리자가 냉장고 관리자와의 채팅방에 접근 가능
    
    수정사항 : 냉장고 관리자가 본인의 관리자 채팅방에 접근 시 예외처리
    
    영향 범위 :

--- src/main/java/com/be16_2nd/SmartFridge/chat/config/StompHandler.java
+++ src/main/java/com/be16_2nd/SmartFridge/chat/config/StompHandler.java
@@ -1,7 +1,6 @@
 package com.be16_2nd.SmartFridge.chat.config;
 
 import com.be16_2nd.SmartFridge.chat.service.ManagerChatRoomLifecycle;
-import com.be16_2nd.SmartFridge.chat.service.ChatService;
 import com.be16_2nd.SmartFridge.chat.service.Validator.ChatRoomParticipantValidator;
 import com.be16_2nd.SmartFridge.chat.service.Validator.TokenValidator;
 import io.jsonwebtoken.Claims;
@@ -26,7 +25,6 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class StompHandler implements ChannelInterceptor {
 
-    private final ChatService chatService;
     private final TokenValidator tokenValidator;
     private final ChatRoomParticipantValidator chatRoomParticipantValidator;
 
