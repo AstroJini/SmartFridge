8937fa57fc29528e921fc28468928ee1a6974d84
YoungKwanK <young.kwin@gmail.com>
2025-09-09 21:12:21 +0900

    [수정]프로젝트 시간대 seoul로 변경

--- src/main/java/com/be16_2nd/SmartFridge/SmartFridgeApplication.java
+++ src/main/java/com/be16_2nd/SmartFridge/SmartFridgeApplication.java
@@ -1,14 +1,22 @@
 package com.be16_2nd.SmartFridge;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableRabbit
 public class SmartFridgeApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartFridgeApplication.class, args);
