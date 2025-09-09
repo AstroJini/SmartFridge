bce588b3aee828ef7f0e8710401dc08d7831edee
YoungKwanK <young.kwin@gmail.com>
2025-09-10 01:10:15 +0900

    fix

--- src/main/java/com/be16_2nd/SmartFridge/SmartFridgeApplication.java
+++ src/main/java/com/be16_2nd/SmartFridge/SmartFridgeApplication.java
@@ -1,27 +1,18 @@
 package com.be16_2nd.SmartFridge;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
-@EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
 @EnableRabbit
 public class SmartFridgeApplication {
-
-	@PostConstruct
-	public void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
-
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(SmartFridgeApplication.class, args);
 	}
-
 }
