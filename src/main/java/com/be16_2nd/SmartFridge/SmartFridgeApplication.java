ba87e49029caea6a49f015612dd23e71df6f508a
YoungKwanK <young.kwin@gmail.com>
2025-09-10 00:20:56 +0900

    JVM시간 설정 및 active: prod로 실행

--- src/main/java/com/be16_2nd/SmartFridge/SmartFridgeApplication.java
+++ src/main/java/com/be16_2nd/SmartFridge/SmartFridgeApplication.java
@@ -4,10 +4,12 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
 @EnableRabbit
