bdd077cd6815076d256cbdc5a3da1002207d44e1
YoungKwanK <young.kwin@gmail.com>
2025-09-09 19:01:08 +0900

    fix

---
+++ src/main/java/com/be16_2nd/SmartFridge/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.be16_2nd.SmartFridge;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
